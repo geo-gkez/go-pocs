33c6a3ff40b4bc4985cc45d7c571b0d327915824
geo-gkez <georgegezeris@gmail.com>
2025-06-08 17:21:56 +0300

    refactor: Remove redundant files and configurations from Redpanda POC

--- go-redpanda/cmd/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/geo-gkez/go-pocs/redpanda-poc/internal/config"
-)
-
-func main() {
-	config.SetupApp()
-}
--- go-redpanda/internal/model/http_models.go
+++
@@ -1,6 +0,0 @@
-package model
-
-type ProduceMessageRequest struct {
-	Key     string `json:"key" binding:"required"`
-	Message string `json:"message" binding:"required"`
-}
